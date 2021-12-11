85a9cbba92fc6bb5f3ec6669984cf2397cf97625
SBTHDR <subrata.halder13d@gmail.com>
2021-12-11 20:21:53 +0600

    Validation Schema

--- controllers/auth/registerController.js
+++ controllers/auth/registerController.js
@@ -1,5 +1,22 @@
+import Joi from 'joi';
+
 const registerController = {
     register(req, res, next) {
+
+        // validation
+        const registerSchema = Joi.object({
+            "name": Joi.string().min(3).max(50).required(),
+            "email": Joi.string().email().required(),
+            "password": Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{6,30}$')).required(),
+            "confirm_password": Joi.ref('password')
+        });
+
+        const { error } = registerSchema.validate(req.body);
+
+        if (error) {
+            return next(error);
+        }
+
         res.json({ msg: "Express Register" });
     }
 }
