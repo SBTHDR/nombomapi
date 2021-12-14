e6e9435a3338f11b8823d9471f808df3180233b9
SBTHDR <subrata.halder13d@gmail.com>
2021-12-14 19:16:03 +0600

    Fetch selected fields

--- controllers/auth/userController.js
+++ controllers/auth/userController.js
@@ -4,7 +4,7 @@ import CustomErrorHandler from "../../services/CustomErrorHandler";
 const userController = {
     async user(req, res, next) {
         try {
-            const user = await User.findOne({ _id: req.user._id });
+            const user = await User.findOne({ _id: req.user._id }).select('-password -updatedAt -__v');
             if (!user) {
                 return next(CustomErrorHandler.notFound());
             }
