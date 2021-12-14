0df93b0e9b1034726aba7a5d24d2b4bbdc709383
SBTHDR <subrata.halder13d@gmail.com>
2021-12-14 20:36:55 +0600

    Refresh token with db whitelist

--- controllers/auth/registerController.js
+++ controllers/auth/registerController.js
@@ -1,8 +1,9 @@
 import Joi from 'joi';
 import CustomErrorHandler from '../../services/CustomErrorHandler';
-import { User } from '../../models';
+import { RefreshToken, User } from '../../models';
 import bcrypt from 'bcrypt';
 import JwtService from '../../services/JwtService';
+import { REFRESH_SECRET } from '../../config';
 
 const registerController = {
     async register(req, res, next) {
@@ -44,19 +45,25 @@ const registerController = {
         });
 
         let access_token;
+        let refresh_token;
         try {
             const result = await user.save();
             console.log(result);
 
             // JWD Token
             access_token =  JwtService.sign({ _id: result._id, role: result.role });
+            refresh_token =  JwtService.sign({ _id: result._id, role: result.role }, '1y', REFRESH_SECRET);
+
+            // Whitelisting Token
+            await RefreshToken.create({ token: refresh_token });
+
 
         } catch (err) {
             return next(err);
         }
 
 
-        res.json({ access_token: access_token });
+        res.json({ access_token: access_token, refresh_token: refresh_token });
     }
 }
 
